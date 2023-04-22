4d6669a83a1982b26f660695ee551572ed9e3eaf
Ed Griebel <edgriebel@users.noreply.github.com>
2023-04-22 19:38:51 -0400

    Assert old and new record counts match

--- src/test/java/Obfuscator.java
+++ src/test/java/Obfuscator.java
@@ -1,7 +1,13 @@
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import java.io.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
@@ -14,17 +20,19 @@ public class Obfuscator {
     private static Logger logger = LogManager.getLogger(Obfuscator.class.getName());
 
     public BankRecords obfuscate(BankRecords rawObjects) {
-        // TODO: Obfuscate and return the records!
-        // Fill these in with your own!
-        BankRecords obfuscatedRecords = new BankRecords(rawObjects.owners(), rawObjects.accounts(), rawObjects.registerEntries());
-        return obfuscatedRecords;
+        // TODO: Obfuscate and return the records! Fill these in with your own
+        Collection<Owner> obfuscatedOwners = rawObjects.owners();
+        Collection<Account> obfuscatedAccounts = rawObjects.accounts();
+        Collection<RegisterEntry> obfuscatedRegisterEntries = rawObjects.registerEntries();
+
+        return new BankRecords(obfuscatedOwners, obfuscatedAccounts, obfuscatedRegisterEntries);
     }
 
     /**
      * Change the integration test suite to point to our obfuscated production
-     * data records.
+     * records.
      *
-     * To get the original integration test suite files run
+     * To use the original integration test suite files run
      *   "git checkout -- src/test/resources/persister_integ.properties"
      */
     public void updateIntegProperties() throws IOException {
@@ -37,6 +45,7 @@ public class Obfuscator {
             props.load(propsStream);
         }
         props.setProperty("persisted.suffix", "_prod");
+        logger.info("Updating properties file '{}'", propsFile);
         try (OutputStream propsStream = new FileOutputStream(propsFile)) {
             String comment = String.format(
                     "Note: Don't check in changes to this file!!\n" +
@@ -48,29 +57,51 @@ public class Obfuscator {
     }
 
     public static void main(String[] args) throws Exception {
+        // enable assertions
+        Obfuscator.class.getClassLoader().setClassAssertionStatus("Obfuscator", true);
         logger.info("Loading Production Records");
         Persister.setPersisterPropertiesFile("persister_prod.properties");
-        Persister.loadPersistedFileNameAndDir();
         Bank bank = new Bank();
         bank.loadAllRecords();
 
         logger.info("Obfuscating records");
         Obfuscator obfuscator = new Obfuscator();
-        BankRecords obfuscatedRecords = obfuscator.obfuscate(new BankRecords(bank.getAllOwners(), bank.getAllAccounts(), bank.getAllRegisterEntries()));
+        // Make a copy of original values so we can compare length
+        // deep-copy collections so changes in obfuscator don't impact originals
+        BankRecords originalRecords = new BankRecords(
+                new ArrayList<>(bank.getAllOwners()),
+                new ArrayList<>(bank.getAllAccounts()),
+                new ArrayList<>(bank.getAllRegisterEntries()));
+        BankRecords obfuscatedRecords = obfuscator.obfuscate(originalRecords);
 
         logger.info("Saving obfuscated records");
         obfuscator.updateIntegProperties();
         Persister.resetPersistedFileNameAndDir();
         Persister.setPersisterPropertiesFile("persister_integ.properties");
-        Persister.loadPersistedFileNameAndDir();
         // FIXME: writeReords is cribbed from Bank.saveALlRecords(), refactor into common method?
         Persister.writeRecordsToCsv(obfuscatedRecords.owners(), "owners");
         Map<Class<? extends Account>, List<Account>> splitAccounts = obfuscatedRecords
                 .accounts()
                 .stream()
                 .collect(Collectors.groupingBy(rec -> rec.getClass()));
-        int savingsCount = Persister.writeRecordsToCsv(splitAccounts.get(SavingsAccount.class), "savings");
-        int checkingCount = Persister.writeRecordsToCsv(splitAccounts.get(CheckingAccount.class),"checking");
+        Persister.writeRecordsToCsv(splitAccounts.get(SavingsAccount.class), "savings");
+        Persister.writeRecordsToCsv(splitAccounts.get(CheckingAccount.class),"checking");
         Persister.writeRecordsToCsv(obfuscatedRecords.registerEntries(), "register");
+
+        logger.info("Original   record counts: {} owners, {} accounts, {} registers",
+                originalRecords.owners().size(),
+                originalRecords.accounts().size(),
+                originalRecords.registerEntries().size());
+        logger.info("Obfuscated record counts: {} owners, {} accounts, {} registers",
+                obfuscatedRecords.owners().size(),
+                obfuscatedRecords.accounts().size(),
+                obfuscatedRecords.registerEntries().size());
+
+        if (obfuscatedRecords.owners().size() != originalRecords.owners().size())
+            throw new AssertionError("Owners count mismatch");
+        if (obfuscatedRecords.accounts().size() != originalRecords.accounts().size())
+            throw new AssertionError("Account count mismatch");
+        if (obfuscatedRecords.registerEntries().size() != originalRecords.registerEntries().size())
+            throw new AssertionError("RegisterEntries count mismatch");
     }
 }
